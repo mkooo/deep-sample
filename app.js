32b7ec9ef34ef1a05ecee794e7d0edcbeef42f04
mkooo <gh-masa@svk.jp>
2019-03-12 21:08:42 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+console.log('hello world :o');
+
+const dreams = [
+  'Find and count some sheep',
+  'Climb a really tall mountain',
+  'Wash the dishes'
+];
+
+const dreamsList = document.getElementById('dreams');
+const dreamsForm = document.forms[0];
+const dreamInput = dreamsForm.elements['dream'];
+
+const appendNewDream = function(dream) {
+  const newListItem = document.createElement('li');
+  newListItem.innerHTML = dream;
+  dreamsList.appendChild(newListItem);
+}
+
+dreams.forEach( function(dream) {
+  appendNewDream(dream);
+});
+
+dreamsForm.onsubmit = function(event) {
+  event.preventDefault();
+
+  dreams.push(dreamInput.value);
+  appendNewDream(dreamInput.value);
+
+  dreamInput.value = '';
+  dreamInput.focus();
+};
